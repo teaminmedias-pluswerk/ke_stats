ad67bf8108cacd899916857b50625b5a1ceb682e
chrisb <chrisb@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-06-08 15:59:06 +0000

    version number update
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/ke_stats/trunk@34024 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "ke_stats"
 #
-# Auto generated 04-06-2010 15:47
+# Auto generated 08-06-2010 17:59
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'Statistics for TYPO3: pageviews/visits, live visitor tracking and extension statistics. Supports fe_users, languages and page types. Support for tt_news and commerce is built in. Easy to adapt to other extensions.',
 	'category' => 'module',
 	'shy' => 0,
-	'version' => '0.5.0',
+	'version' => '0.5.1',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
@@ -40,7 +40,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:37:{s:9:"ChangeLog";s:4:"6623";s:10:"README.txt";s:4:"c135";s:21:"ext_conf_template.txt";s:4:"db0a";s:12:"ext_icon.gif";s:4:"3261";s:17:"ext_localconf.php";s:4:"7475";s:14:"ext_tables.php";s:4:"cad8";s:14:"ext_tables.sql";s:4:"9420";s:24:"ext_typoscript_setup.txt";s:4:"0df6";s:28:"icon_tx_kestats_statdata.gif";s:4:"475a";s:16:"locallang_db.xml";s:4:"9570";s:7:"tca.php";s:4:"89db";s:16:"userTSconfig.txt";s:4:"8cf0";s:26:"mod1/backendmenu.class.php";s:4:"7160";s:14:"mod1/clear.gif";s:4:"cc11";s:13:"mod1/conf.php";s:4:"955c";s:14:"mod1/index.php";s:4:"e16f";s:18:"mod1/locallang.xml";s:4:"6bff";s:22:"mod1/locallang_mod.xml";s:4:"bfa9";s:19:"mod1/moduleicon.gif";s:4:"3261";s:28:"lib/class.tx_kestats_lib.php";s:4:"510c";s:26:"flotr/flotr-0.2.0-alpha.js";s:4:"f6a2";s:19:"flotr/lib/base64.js";s:4:"dc32";s:25:"flotr/lib/canvas2image.js";s:4:"ba51";s:23:"flotr/lib/canvastext.js";s:4:"9a59";s:21:"flotr/lib/excanvas.js";s:4:"fe7d";s:30:"flotr/lib/prototype-1.6.0.2.js";s:4:"85ef";s:14:"doc/manual.sxw";s:4:"b6a8";s:19:"doc/wizard_form.dat";s:4:"fe60";s:20:"doc/wizard_form.html";s:4:"5120";s:20:"inc/browsers.inc.php";s:4:"9f16";s:21:"inc/constants.inc.php";s:4:"d953";s:29:"inc/operating_systems.inc.php";s:4:"67ac";s:18:"inc/robots.inc.php";s:4:"9aa5";s:26:"inc/search_engines.inc.php";s:4:"f7b9";s:12:"cli/conf.php";s:4:"9d24";s:18:"cli/update.cli.php";s:4:"f6fb";s:28:"pi1/class.tx_kestats_pi1.php";s:4:"8033";}',
+	'_md5_values_when_last_written' => 'a:37:{s:9:"ChangeLog";s:4:"6623";s:10:"README.txt";s:4:"c135";s:21:"ext_conf_template.txt";s:4:"db0a";s:12:"ext_icon.gif";s:4:"3261";s:17:"ext_localconf.php";s:4:"7475";s:14:"ext_tables.php";s:4:"cad8";s:14:"ext_tables.sql";s:4:"9420";s:24:"ext_typoscript_setup.txt";s:4:"0df6";s:28:"icon_tx_kestats_statdata.gif";s:4:"475a";s:16:"locallang_db.xml";s:4:"9570";s:7:"tca.php";s:4:"89db";s:16:"userTSconfig.txt";s:4:"8cf0";s:26:"mod1/backendmenu.class.php";s:4:"7160";s:14:"mod1/clear.gif";s:4:"cc11";s:13:"mod1/conf.php";s:4:"955c";s:14:"mod1/index.php";s:4:"e16f";s:18:"mod1/locallang.xml";s:4:"6bff";s:22:"mod1/locallang_mod.xml";s:4:"bfa9";s:19:"mod1/moduleicon.gif";s:4:"3261";s:28:"lib/class.tx_kestats_lib.php";s:4:"d232";s:26:"flotr/flotr-0.2.0-alpha.js";s:4:"f6a2";s:19:"flotr/lib/base64.js";s:4:"dc32";s:25:"flotr/lib/canvas2image.js";s:4:"ba51";s:23:"flotr/lib/canvastext.js";s:4:"9a59";s:21:"flotr/lib/excanvas.js";s:4:"fe7d";s:30:"flotr/lib/prototype-1.6.0.2.js";s:4:"85ef";s:14:"doc/manual.sxw";s:4:"0d50";s:19:"doc/wizard_form.dat";s:4:"fe60";s:20:"doc/wizard_form.html";s:4:"5120";s:20:"inc/browsers.inc.php";s:4:"9f16";s:21:"inc/constants.inc.php";s:4:"d953";s:29:"inc/operating_systems.inc.php";s:4:"67ac";s:18:"inc/robots.inc.php";s:4:"9aa5";s:26:"inc/search_engines.inc.php";s:4:"f7b9";s:12:"cli/conf.php";s:4:"9d24";s:18:"cli/update.cli.php";s:4:"f6fb";s:28:"pi1/class.tx_kestats_pi1.php";s:4:"8033";}',
 	'suggests' => array(
 	),
 );

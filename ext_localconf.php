63f1751015307721d7ad91fc5d7109408e3463eb
chrisb <chrisb@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-11-17 10:14:41 +0000

    File access counting
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/ke_stats/trunk@40320 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_localconf.php
+++ ext_localconf.php
@@ -12,4 +12,6 @@ if (!defined ('PATH_BE_KESTATS')) {
 t3lib_extMgm::addUserTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:ke_stats/userTSconfig.txt">');
 
 t3lib_extMgm::addPItoST43($_EXTKEY,'pi1/class.tx_kestats_pi1.php','_pi1','',0);
+
+$GLOBALS['TYPO3_CONF_VARS']['FE']['eID_include']['tx_kestats_filecounter'] = 'EXT:ke_stats/filecounter/index.php';
 ?>
